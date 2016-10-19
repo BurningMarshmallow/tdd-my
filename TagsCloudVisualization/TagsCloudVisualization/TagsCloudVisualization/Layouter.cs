6434dfb1ce1f0fe8832f2f6ff03772651775a872
BurningMarshmallow <stanislav-fedjanin@rambler.ru>
2016-10-19 23:57:54 +0500

    Initial commit

---
+++ TagsCloudVisualization/TagsCloudVisualization/TagsCloudVisualization/Layouter.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public class CircularCloudLayouter
+    {
+        private Point center;
+        public CircularCloudLayouter(Point center)
+        {
+            this.center = center;
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            return new Rectangle(center, rectangleSize);
+        }
+    }
+}
---
+++ TagsCloudVisualization/TagsCloudVisualization/TagsCloudVisualization/Layouter_Tests.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization
+{
+    [TestFixture]
+    class Layouter_Tests
+    {
+        [Test]
+        public void AddCentralWord()
+        {
+            var layouter = new CircularCloudLayouter(new Point(100, 100));
+            var placeToPut = layouter.PutNextRectangle(new Size(4, 4));
+            Assert.AreEqual(new Rectangle(100, 100, 4, 4), placeToPut);
+        }
+    }
+}
